f5a869bf7f3d9b5fa710328b5d251d60cbf148f7
lixiaotao <lixiaotao19901103@gmail.com>
2016-03-30 17:22:34 +0800

    Initial Commit

---
+++ AdaptDemoAnotherWayTests/AdaptDemoAnotherWayTests.m
@@ -0,0 +1,39 @@
+//
+//  AdaptDemoAnotherWayTests.m
+//  AdaptDemoAnotherWayTests
+//
+//  Created by john on 16/3/30.
+//  Copyright © 2016年 jhon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AdaptDemoAnotherWayTests : XCTestCase
+
+@end
+
+@implementation AdaptDemoAnotherWayTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
